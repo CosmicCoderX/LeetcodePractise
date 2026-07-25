df3200b04b34e10566f2442ccd2cfde616c2014a
Suryakant Dwivedi <147908822+CosmicCoderX@users.noreply.github.com>
2026-07-25 18:02:24 +0530

    Time: 10 ms (8.83%) | Memory: 59.5 MB (28.64%) - LeetSync

---
+++ 335-self-crossing/self-crossing.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isSelfCrossing(int[] distance) {
+        int n=distance.length;
+        for(int i=3; i<n; i++){
+            //Case-1: Normal crossing with i-3
+            if(distance[i] >= distance[i-2] && 
+            distance[i-1] <= distance[i-3]){
+                return true;
+            }
+
+            //Case 2: Touching/overlapping with i-4
+            if(i >=4 &&
+            distance[i-1] == distance[i-3] && 
+            distance[i] + distance[i-4] >= distance[i-2]){
+                return true;
+            }
+
+            //Case 3: Crossing after the spiral starts shrinking, involving i-5
+            if(i>=5 &&
+            distance[i-2] >= distance[i-4] && 
+            distance[i] >= distance[i-2]-distance[i-4] && 
+            distance[i-1] >= distance[i-3]-distance[i-5] && 
+            distance[i-1] <= distance[i-3]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
