8206caa8e8b55b864dc1a3a77af37dcbf2789d31
Suryakant Dwivedi <147908822+CosmicCoderX@users.noreply.github.com>
2026-07-26 16:27:17 +0530

    Time: 2 ms (99.61%) | Memory: 47.5 MB (79.80%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,33 +1,32 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        int n = nums.length;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
 
-        int max = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;
-        int thirdMax = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        int secondMin = Integer.MAX_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
 
-        for(int i=0; i<n; i++){
-            if(nums[i] < min){
-                secondMin = min;
-                min = nums[i];
-            }else if(nums[i] < secondMin){
-                secondMin = nums[i];
+        for(int num: nums){
+            if(num <= min1){
+                min2 = min1;
+                min1 = num;
+            }else if(num < min2){
+                min2 = num;
             }
 
-            if(nums[i] > max){
-                thirdMax = secondMax;
-                secondMax = max;
-                max = nums[i];
-            }else if(nums[i] > secondMax){
-                thirdMax = secondMax;
-                secondMax = nums[i];
-            }else if(nums[i] > thirdMax){
-                thirdMax = nums[i];
+            if(num >= max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }else if(num >= max2){
+                max3 = max2;
+                max2 = num;
+            }else if(num > max3){
+                max3 = num;
             }
         }
 
-        return Math.max(max*secondMax*thirdMax, min*secondMin*max);
+        return Math.max(max1*max2*max3, min1*min2*max1);
     }
 }
