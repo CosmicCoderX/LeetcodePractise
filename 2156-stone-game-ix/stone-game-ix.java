b8747ee9c0f1fc1d4a49fb9e0d3e05fe59a9a3f0
Suryakant Dwivedi <147908822+CosmicCoderX@users.noreply.github.com>
2026-08-17 04:12:26 +0530

    Time: 8 ms (12.58%) | Memory: 114.7 MB (64.90%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.java
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -1,23 +1,15 @@
 class Solution {
     public boolean stoneGameIX(int[] stones) {
-        int n = stones.length;
         int c0 = 0, c1 = 0, c2 = 0;
-        for(int i=0; i<n; i++){
-            if(stones[i]%3 == 0) c0++;
-            if(stones[i]%3 == 1) c1++;
-            if(stones[i]%3 == 2) c2++;
+        for(int s: stones){
+            if(s%3 == 0) c0++;
+            if(s%3 == 1) c1++;
+            if(s%3 == 2) c2++;
         }
 
         if(c0%2 == 0){
-            if(c1>0 && c2>0){
-                return true;
-            }
-        }else{
-            if(Math.abs(c1-c2) > 2){
-                return true;
-            }
+            return c1> 0 && c2>0;
         }
-
-        return false;
+        return Math.abs(c1-c2) > 2;
     }
 }
