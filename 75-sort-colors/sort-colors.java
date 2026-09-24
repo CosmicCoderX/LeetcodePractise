d9d12603b5c7ff65e8d06468a83e8cb879760295
Suryakant Dwivedi <147908822+CosmicCoderX@users.noreply.github.com>
2026-09-25 03:35:39 +0530

    Time: 1 ms (17.40%) | Memory: 43.3 MB (88.13%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,12 +1,12 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n-i-1; j++){
-                if(nums[j] > nums[j+1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
+        //Use Bubble Sort
+        for(int i=0; i< nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] > nums[j]){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
                 }
             }
         }
